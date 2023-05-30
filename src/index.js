ff15c355b174df980f85426d459cfee29cebcf87
IronForce-Auscent <discordgamer.070910@gmai.com>
2023-05-30 13:42:51 +0800

    Experimenting with possible changes to fix issue

---
+++ src/index.js
@@ -0,0 +1,6 @@
+var testButton = document.getElementById("testButton")
+
+testButton.addEventListener("click", function () {
+    // redirect the user to another page
+    window.location.href = "about.html"
+})
