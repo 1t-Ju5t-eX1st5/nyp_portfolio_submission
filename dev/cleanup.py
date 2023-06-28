1b663b5ca5e10dfbabf3eb2ac45aa71145916db0
IronForce-Auscent <64062496+IronForce-Auscent@users.noreply.github.com>
2023-06-28 12:34:47 +0000

    Deploying to gh-pages from @ 1t-Ju5t-eX1st5/test_github_pages@26de6943de30f5f7a2d2a11d5d5fe7c48be523c9 🚀

---
+++ dev/cleanup.py
@@ -0,0 +1,17 @@
+import os
+
+def cleanup(files):
+    for file in files:
+        try:
+            if os.path.exists(file):
+                os.remove(file)
+            else:
+                print(f"File not found, skipping\nSkipped file: {file}")
+        except Exception as e:
+            print(f"Error thrown: {e}")
+            continue
+    print("Cleanup complete")
+
+if __name__ == "__main__":
+    files = ['about.html', 'index.html', 'portfolio.html', 'portfolio_website.html']
+    cleanup(files)
