10ba3a996dfc2ae3747b7514cd1cf6ea2f063dae
IronForce-Auscent <discordgamer.070910@gmai.com>
2023-06-28 19:54:52 +0800

    Fixed build.py not being added to the repository

---
+++ build.py
@@ -0,0 +1,26 @@
+from jinja2 import Environment, PackageLoader, select_autoescape
+from jinja2 import exceptions
+
+def build(files: list):
+    env = Environment(
+        loader=PackageLoader("build"),
+        autoescape=select_autoescape()
+    )
+    err_files = []
+    for file in files:
+        try:
+            template = env.get_template(file)
+            with open(file, "w") as f:
+                f.write(template.render())
+        except exceptions.TemplateNotFound as TemplateNotFound:
+            print(f"Error: HTML file \'{file}\' not found. Continuing to render other files...")
+            err_files.append(file)
+            continue
+    if len(err_files) <= 0:
+        print("All files rendered successfully!")
+    else:
+        print(f"The following files were not found/were not able to be rendered: {err_files}")
+
+if __name__ == "__main__":
+    files = ['about.html', 'index.html', 'portfolio_website.html', 'portfolio.html']
+    build(files)
