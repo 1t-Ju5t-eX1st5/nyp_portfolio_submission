ba054cd20e2f494a57727550a50673cd3db7a562
Solitude Nox <64062496+IronForce-Auscent@users.noreply.github.com>
2023-07-07 07:14:52 +0000

    Edited build.py to include new page

--- build.py
+++ build.py
@@ -24,5 +24,5 @@ def build(files: list):
 
 
 if __name__ == "__main__":
-    files = ['about.html', 'index.html', 'portfolio_website.html', 'portfolio.html']
+    files = ['about.html', 'index.html', 'portfolio_website.html', 'portfolio.html', 'participation.html']
     build(files)
